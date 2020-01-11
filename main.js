b2d9b5a5263355eab331130c1ad3a7f84dbcff16
James James <jamesjay4199@gmail.com>
2020-01-11 22:25:06 +0100

    fix issues

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
-        let btn = document.querySelector("button");
-        btn.addEventListener("click", countDown);
-               
-        let hours = "0";
-        let minutes = "0";
-        let seconds =  "10";
+let btn = document.querySelector("button");
+btn.addEventListener("click", countDown);
 
-        hours < 10 ? hours = '0' + hours : hours = hours;
-        minutes < 10 ? minutes = '0' + minutes : minutes = minutes;
-        seconds < 10 ? seconds = '0' + seconds : seconds = seconds;
-        
-        let countContainer = document.getElementById('count');
+let hours = "0";
+let minutes = "0";
+let seconds =  "10";
 
-        
-        function countDown(){       
-            
-             setInterval(() => {
-                let watch = hours + ':' + minutes + ':' + seconds;
-                seconds!="00" ?
-                seconds-- :
-                seconds = '00';
-                countContainer.innerHTML = watch;
-                clearInterval();
-             }, 1000);
+hours < 10 ? hours = '0' + hours : hours = hours;
+minutes < 10 ? minutes = '0' + minutes : minutes = minutes;
+seconds < 10 ? seconds = '0' + seconds : seconds = seconds;
 
+let countContainer = document.getElementById('count');
 
-             function stopInterval(){
-                window.clearInterval();
-            }
-                    
-            }
-            
+function countDown() {
+   let thisInterval = setInterval(() => {
+   let watch = hours + ':' + minutes + ':' + seconds;
+   countContainer.innerHTML = watch;
+   if(Number(seconds) === 0) {
+      clearInterval(thisInterval);
+   }
+   if(seconds > 10) {
+      seconds -= 1;
+   } else {
+      seconds = Number(seconds) -1;
+      seconds = `0${seconds}`;
+   }
+
+   }, 1000);
+}
